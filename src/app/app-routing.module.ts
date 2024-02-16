178673d815debb9a15575928a8135fc9d7afda12
Onodwa2001 <onodwasiyotula7@gmail.com>
2024-02-16 02:28:51 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewsfeedComponent } from './components/pages/newsfeed/newsfeed.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { ProfileComponent } from './components/pages/profile/profile.component';
+import { RegisterComponent } from './components/pages/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/feed',
+    pathMatch: 'full'
+  },
+  {
+    path: 'feed',
+    component: NewsfeedComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewsfeedComponent } from './components/pages/newsfeed/newsfeed.component';
+import { ProfileComponent } from './components/pages/profile/profile.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { RegisterComponent } from './components/pages/register/register.component';
+import { ButtonComponent } from './components/elements/button/button.component';
+import { InputBoxComponent } from './components/elements/input-box/input-box.component';
+import { NavComponent } from './components/content/nav/nav.component';
+import { PostComponent } from './components/content/post/post.component';
+import { CommentComponent } from './components/content/comment/comment.component';
+import { LoginFormComponent } from './components/content/login-form/login-form.component';
+import { SignupFormComponent } from './components/content/signup-form/signup-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsfeedComponent,
+    ProfileComponent,
+    LoginComponent,
+    RegisterComponent,
+    ButtonComponent,
+    InputBoxComponent,
+    NavComponent,
+    PostComponent,
+    CommentComponent,
+    LoginFormComponent,
+    SignupFormComponent
   ],
   imports: [
     BrowserModule,
