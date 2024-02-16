34077b5568ae38751624952b48cc779332d02624
Onodwa2001 <onodwasiyotula7@gmail.com>
2024-02-16 11:55:26 +0200

    Added scroll effect on posts and made posts dynamic

--- src/app/components/content/post/post.component.ts
+++ src/app/components/content/post/post.component.ts
@@ -1,15 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
-export class PostComponent {
-
+export class PostComponent implements OnInit {
+  
   inputStyle: {} = {
     "padding": "15px",
     "width": "300px"
+  }
+
+  @Input() data = {
+    name: '',
+    date: '',
+    message: '',
+    votes: 0,
+    comments: 0
+  };
+
+  @ViewChild('postMessage', { static: true }) myElement!: ElementRef;
+
+  ngOnInit(): void {
+    if (this.data.message.length > 70) {
+      this.myElement.nativeElement.style.fontSize = '15px';
+      this.myElement.nativeElement.style.lineHeight = '1.6em';
+      console.log(this.myElement);
+    }
   }
 
 }
--- src/app/components/pages/newsfeed/newsfeed.component.ts
+++ src/app/components/pages/newsfeed/newsfeed.component.ts
@@ -7,6 +7,21 @@ import { Component } from '@angular/core';
 })
 export class NewsfeedComponent {
 
-  
+  posts: Array<any> = [
+    {
+      name: 'dfdjsgk',
+      date: '20 March 2023',
+      message: 'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available.',
+      votes: 0,
+      comments: 0
+    },
+    {
+      name: 'test',
+      date: '10 February 2024',
+      message: 'Hello There',
+      votes: 1,
+      comments: 0
+    },
+  ];
 
 }
